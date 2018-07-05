49620d35096bfa381d8a31a946c060a75eb7e5c8
jonathan <18895322032@163.com>
2018-07-05 17:14:12 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'wy-vdialog.js',
+    filename: 'wy-vdialog.min.js',
     library: 'vDialog',
     libraryTarget: 'umd',
     umdNamedDefine: true
